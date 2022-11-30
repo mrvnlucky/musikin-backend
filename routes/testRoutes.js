a0a7978d776f19c004c27337b000d50fbf08a5fa
Marvin <marvnluck@gmail.com>
2022-12-01 00:08:29 +0700

    add error status to test route

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -4,9 +4,15 @@ const auth = require("../middleware/auth.js")
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.status(200).send("hello world")
+  try {
+    res.status(200).send(
+      "Hello World"
+    )
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occured while getting owner"
+    })
+  }
+})
 
-}
-
-)
 module.exports = router;
